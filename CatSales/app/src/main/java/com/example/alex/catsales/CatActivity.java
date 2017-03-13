aa8490ab04bd8362e14099fc40f7b52b62af630c
Alexander Orlov <79202875363@ya.ru>
2017-03-13 16:15:01 +0300

    Заполнил Класс CatActivity, подключил кликЛиссенер.

--- CatSales/app/src/main/java/com/example/alex/catsales/CatActivity.java
+++ CatSales/app/src/main/java/com/example/alex/catsales/CatActivity.java
@@ -2,6 +2,7 @@ package com.example.alex.catsales;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,7 +11,7 @@ import android.widget.TextView;
  * Created by alex on 13.03.17.
  */
 
-public class CatActivity extends Activity {
+public class CatActivity extends Activity implements View.OnClickListener {
     private ImageView ivPhoto;
     private TextView tvName, tvBreed, tvDescription;
     private Button bCall;
@@ -20,10 +21,37 @@ public class CatActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat);
         initView();
+        setImageView();
+        setTextView();
+        buttonBehavior();
     }
 
     private void initView() {
+        ivPhoto = (ImageView)findViewById(R.id.photo);
+        tvName = (TextView)findViewById((R.id.name));
+        tvBreed = (TextView)findViewById(R.id.breed);
+        tvDescription = (TextView)findViewById(R.id.description);
+        bCall = (Button)findViewById(R.id.call);
+    }
+
+    private void setImageView(){
 
     }
 
+    private void setTextView(){
+        
+    }
+
+    private void buttonBehavior(){
+        bCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.call:
+                //здесь метод вызова звонка
+                break;
+        }
+    }
 }
