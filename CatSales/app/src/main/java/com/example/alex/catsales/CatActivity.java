3fc9126ccaf607b0c3c4f84765e25136dddf8d6a
Alexander Orlov <79202875363@ya.ru>
2017-03-13 15:41:56 +0300

    Создал layout для CatActivity

--- CatSales/app/src/main/java/com/example/alex/catsales/CatActivity.java
+++ CatSales/app/src/main/java/com/example/alex/catsales/CatActivity.java
@@ -1,10 +1,29 @@
 package com.example.alex.catsales;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by alex on 13.03.17.
  */
 
 public class CatActivity extends Activity {
+    private ImageView ivPhoto;
+    private TextView tvName, tvBreed, tvDescription;
+    private Button bCall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
 }
