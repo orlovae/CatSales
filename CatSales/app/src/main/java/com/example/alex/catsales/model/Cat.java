f640a3db6471637ffee7bc8b6c6fddb51510d72a
Alexander Orlov <79202875363@ya.ru>
2017-03-13 16:07:00 +0300

    Заполнил layout activity_cat

--- CatSales/app/src/main/java/com/example/alex/catsales/model/Cat.java
+++ CatSales/app/src/main/java/com/example/alex/catsales/model/Cat.java
@@ -6,11 +6,18 @@ package com.example.alex.catsales.model;
 
 public class Cat {
     private String uriPhoto, name, breed, description;
+    private double latitude, longtude;
+    private int telefon;
 
-    public Cat(String uriPhoto, String name, String breed, String description) {
+    public Cat(String uriPhoto, String name, String breed, String description,
+               double latitude, double longtude, int telefon) {
         this.uriPhoto = uriPhoto;
         this.name = name;
         this.breed = breed;
         this.description = description;
+        this.latitude = latitude;
+        this.longtude = longtude;
+        this.telefon = telefon;
     }
+
 }
