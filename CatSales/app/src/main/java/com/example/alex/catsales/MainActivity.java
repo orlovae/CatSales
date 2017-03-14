682b9d61c689d9594e31b507be7c1bb4a044ee85
Alexander Orlov <79202875363@ya.ru>
2017-03-14 12:14:27 +0300

    Подключил GoogleMaps

--- CatSales/app/src/main/java/com/example/alex/catsales/MainActivity.java
+++ CatSales/app/src/main/java/com/example/alex/catsales/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.alex.catsales;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createMapView();
+
+    }
+
+    private void createMapView(){
+        final MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
     }
 }
