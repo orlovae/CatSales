d1dc8d9fab6f3abe43ef4e1bce3f3a7bc69d34ee
Alexander Orlov <79202875363@ya.ru>
2017-03-15 13:00:42 +0300

    Теперь данные берутся из БД

--- CatSales/app/src/main/java/com/example/alex/catsales/MyApp.java
+++ CatSales/app/src/main/java/com/example/alex/catsales/MyApp.java
@@ -1,12 +1,18 @@
 package com.example.alex.catsales;
 
 import android.app.Application;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import com.example.alex.catsales.helper.DBHelper;
 import com.example.alex.catsales.model.Cat;
 import com.example.alex.catsales.model.FakeCatArray;
+import com.example.alex.catsales.table.FakeCatTable;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import static com.example.alex.catsales.CatActivity.TAG;
 
@@ -15,8 +21,10 @@ import static com.example.alex.catsales.CatActivity.TAG;
  */
 
 public class MyApp extends Application {
-    private FakeCatArray db;
+    private FakeCatArray fakeCatArray;
     private ArrayList<Cat> fakeListCat;
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
 
     public ArrayList<Cat> getFakeListCat() {
         return fakeListCat;
@@ -26,43 +34,94 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        fabriceCat(initDB());
+        initDBHelper();
+
+        initFakeCatArray();
+
+        initFakeCatTable();
+
+        initfakeListCat();
     }
 
-    private FakeCatArray initDB(){
+    private void initDBHelper(){
+        dbHelper = new DBHelper(this);
+    }
+
+    private void initFakeCatArray(){
         Log.i(TAG, "Start initDB");
-        if (db == null){
-            db = new FakeCatArray();
-            return db;
+        if (fakeCatArray == null){
+            fakeCatArray = new FakeCatArray();
         }
-        return db;
     }
-
-    private ArrayList<Cat> fabriceCat(FakeCatArray db){
-        Log.i(TAG, "Start fabriceCat");
-        if(fakeListCat == null) {
-            fakeListCat = new ArrayList<Cat>();
-        }
+    public void initFakeCatTable(){
+        db = dbHelper.getWritableDatabase();
 
         int ID;
         String uriPhoto, name, breed, description;
         double latitude, longtude;
         String telefon;
 
-        for (int i = 0; i < db.getName().size(); i++) {
+        for (int i = 0; i < fakeCatArray.getName().size(); i++) {
             ID = i;
             uriPhoto = "";
-            name = db.getName().get(i);
-            breed = db.getBreed().get(i);
-            description = db.getDescription().get(i);
-            latitude = db.getLatitude().get(i);
-            longtude = db.getLongtude().get(i);
-            telefon = db.getTelefon().get(i);
+            name = fakeCatArray.getName().get(i);
+            breed = fakeCatArray.getBreed().get(i);
+            description = fakeCatArray.getDescription().get(i);
+            latitude = fakeCatArray.getLatitude().get(i);
+            longtude = fakeCatArray.getLongtude().get(i);
+            telefon = fakeCatArray.getTelefon().get(i);
 
-            Cat cat = new Cat(ID, uriPhoto, name, breed, description, latitude, longtude, telefon);
-            fakeListCat.add(cat);
+            ContentValues cV = new ContentValues();
+            cV.put(FakeCatTable.COLUMN_ID, i);
+            cV.put(FakeCatTable.COLUMN_URI_PHOTO, uriPhoto);
+            cV.put(FakeCatTable.COLUMN_NAME, name);
+            cV.put(FakeCatTable.COLUMN_BREED, breed);
+            cV.put(FakeCatTable.COLUMN_DESCRIPTION, description);
+            cV.put(FakeCatTable.COLUMN_LATITUDE, latitude);
+            cV.put(FakeCatTable.COLUMN_LONGTUDE, longtude);
+            cV.put(FakeCatTable.COLUMN_TELEFON, telefon);
+            long rowID = db.insert(FakeCatTable.TABLE_NAME, null, cV);
         }
-
-        return fakeListCat;
+        dbHelper.close();
     }
+
+    private void initfakeListCat(){
+        db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.query(FakeCatTable.TABLE_NAME, null, null, null, null, null, null);
+
+        fakeListCat = FakeCatTable.getResultFromCursor(cursor);
+
+        if(cursor != null) cursor.close();
+        dbHelper.close();
+    }
+
+
+
+//    private ArrayList<Cat> fabriceCat(FakeCatArray db){
+//        Log.i(TAG, "Start fabriceCat");
+//        if(fakeListCat == null) {
+//            fakeListCat = new ArrayList<Cat>();
+//        }
+//
+//        int ID;
+//        String uriPhoto, name, breed, description;
+//        double latitude, longtude;
+//        String telefon;
+//
+//        for (int i = 0; i < db.getName().size(); i++) {
+//            ID = i;
+//            uriPhoto = "";
+//            name = db.getName().get(i);
+//            breed = db.getBreed().get(i);
+//            description = db.getDescription().get(i);
+//            latitude = db.getLatitude().get(i);
+//            longtude = db.getLongtude().get(i);
+//            telefon = db.getTelefon().get(i);
+//
+//            Cat cat = new Cat(ID, uriPhoto, name, breed, description, latitude, longtude, telefon);
+//            fakeListCat.add(cat);
+//        }
+//
+//        return fakeListCat;
+//    }
 }
---
+++ CatSales/app/src/main/java/com/example/alex/catsales/helper/DBHelper.java
@@ -0,0 +1,30 @@
+package com.example.alex.catsales.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.alex.catsales.table.FakeCatTable;
+
+/**
+ * Created by alex on 15.03.17.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "DBFakeCat";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        FakeCatTable.createTable(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
